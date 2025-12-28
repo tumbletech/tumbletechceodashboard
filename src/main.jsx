c047cc1bb68e1760c34ff6552e5f1eb7a07c1c38
Tumbletech, Inc. <112755396+tumbletech@users.noreply.github.com>
2025-12-28 21:46:29 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  document.body.innerHTML = "<h1 style='color:red'>ROOT NOT FOUND</h1>";
-}
-
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
